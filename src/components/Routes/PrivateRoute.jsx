9b9b3b606f07e851f42e058900491130c0ef248d
manishshukla00 <132373386+manishshukla00@users.noreply.github.com>
2024-04-18 12:13:56 +0530

    Update PrivateRoute.jsx

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -11,7 +11,7 @@ const PrivateRoute = () => {
   useEffect(() => {
     const authCheck = async () => {
       const res = await axios.get(
-        "http://localhost:8080/api/v1/auth/user-auth",
+        "https://e-commerce-backend-gn5p.onrender.com/api/v1/auth/user-auth",
         { headers: { Authorization: auth?.token } }
       );
       if (res.data.ok) {
